ae020825040702f792f8b2e16ef263cb97d0e780
古川広一 <ex.kouichi1224@gmail.com>
2021-07-17 15:33:07 +0900

    prisma client を導入

---
+++ index.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  await createUser;
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  });
+  console.log(post);
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+async function createUser() {
+  const data = {
+    name: "Alice",
+    email: "alice@prisma.io",
+    posts: {
+      create: { title: "Hello World" },
+    },
+    profile: {
+      create: { bio: "I like turtles" },
+    },
+  };
+  await prisma.user.create({
+    data,
+  });
+}
